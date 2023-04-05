290a297c88716787cec8c99ff7171f87fda5fe86
KayongoYongo <sknsammie10@gmail.com>
2023-04-05 17:00:16 +0000

    set up web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#Sets up a web server for deployment by ngin
+
+#Install ngingx if not already installed
+sudo apt-get update
+sudo apt-get install -y nginx
+
+#Create the directories even if they don't exist
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+#create a fake HTML file with simple content
+echo "Holberton School" > /data/web_static/releases/test/index.html
+
+#Create a symbolic link. If it exists, it should be deleted and recreated.
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#Change the owner and the group of the data folder
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+#update nginx configuration
+
+#Restart nginx
+service nginx restart
